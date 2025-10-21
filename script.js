65fceb2f731f7ac8bdc9f237e0d35f94982ec8de
mariaantunesalves-ui <maria.antunes.alves@escola.pr.gov.br>
2025-10-21 09:33:39 -0300

    21/10

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave(){
+function mostraPalavrasChave() {
     alert("Fui clicado!");
 }
